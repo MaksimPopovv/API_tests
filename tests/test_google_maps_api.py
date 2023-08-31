5d2bee8e32294794b90e932b8dd659aa3f33c7b4
Max <max21wq@mail.ru>
2023-08-31 20:14:54 +0500

    Add tests

---
+++ tests/test_google_maps_api.py
@@ -0,0 +1,54 @@
+import allure
+from utils.api import Google_maps_api
+from utils.checking import Checking
+
+
+@allure.epic("Test create place")
+class Test_create_place:
+    """Создание, изменение и удаление новой локации"""
+
+    @allure.description("Test create, update, delete new place")
+    def test_create_new_place(self):
+        print('\nМетод POST')
+        result_post = Google_maps_api.create_new_place()
+        place_id = result_post.json().get('place_id')
+        Checking.check_status_code(result_post, 200)
+        Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
+        Checking.check_json_value(result_post, 'status', 'OK')
+
+        print("\nМетод GET POST")
+        result_get = Google_maps_api.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get,
+                                  ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website',
+                                   'language'])
+        Checking.check_json_value(result_get, 'address', '29, side layout, cohen 09')
+
+        print('\nМетод PUT')
+        result_put = Google_maps_api.put_new_place(place_id)
+        Checking.check_status_code(result_put, 200)
+        Checking.check_json_token(result_put, ['msg'])
+        Checking.check_json_value(result_put, 'msg', 'Address successfully updated')
+
+        print("\nМетод GET PUT")
+        result_get = Google_maps_api.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get,
+                                  ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website',
+                                   'language'])
+        Checking.check_json_value(result_get, 'address', '100 Lenina street, RU')
+
+        print("\nМетод DELETE")
+        result_delete = Google_maps_api.delete_new_place(place_id)
+        Checking.check_status_code(result_delete, 200)
+        Checking.check_json_token(result_delete, ['status'])
+        Checking.check_json_value(result_delete, 'status', 'OK')
+
+        print("\nМетод GET DELETE")
+        result_get = Google_maps_api.get_new_place(place_id)
+        Checking.check_status_code(result_get, 404)
+        Checking.check_json_token(result_get, ['msg'])
+        Checking.check_json_value(result_get, 'msg', "Get operation failed, looks like place_id  doesn't exists")
+
+        print(
+            '-------------------------------------------------------------------------\nТестирование прошло успешно!!!')
